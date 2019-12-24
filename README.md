ef525a1126c13c91b252cbba1630edcb4ab15eae
rulli-aji-gunawan <rully.okayama@gmail.com>
2019-12-24 13:31:33 +0700

    revisi program

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-Latihan 5
+Tugas Praktikum 5
+  
+  
+Langkah Pengerjaan :  
+1. Buat Flowchart ALgoritma  
+2. Buat dictionary kosong dan penomoran daftar dimulai dari 0  
+  
+![EmptyDictionary](https://user-images.githubusercontent.com/56189248/70879238-2c5e9280-1ff7-11ea-8b52-15672f94b9f8.png)Welcome to the Labspy05 wiki!  
+  
+3. Gunakan printah perulangan dan dan statement if untuk opsi perintah  
+4. Untuk program opsi perintah (K)eluar , (L)ihat , (T)ambah, gunakan program seperti berikut : 
+  
+![KeluarLihatTambah](https://user-images.githubusercontent.com/56189248/70880113-61b8af80-1ffa-11ea-9f17-9d1ea48b2a0f.png)  
+  
+5. Untuk program opsi perintah (U)bah dan (H)apus, gunakan program seperti berikut : 
+  
+![UbahHapus](https://user-images.githubusercontent.com/56189248/70880294-076c1e80-1ffb-11ea-8b2c-580ac183a28c.png)  
+  
+6. Untuk program opsi perintah (C)ari, gunakan program seperti berikut :  
+  
+![Cari](https://user-images.githubusercontent.com/56189248/70880500-c1638a80-1ffb-11ea-9d19-207d853fb733.png) 
+  
+Berikut contoh hasil output untuk opsi (T)ambah dan (L)ihat :  
+  
+![Output1](https://user-images.githubusercontent.com/56189248/70880813-edcbd680-1ffc-11ea-9138-dfa33ec51eef.png)  
+  
+Berikut contoh hasil output untuk opsi (U)bah :  
+  
+![OutputUbah](https://user-images.githubusercontent.com/56189248/70880979-967a3600-1ffd-11ea-9097-eaec469aa3b5.png)  
+  
+Berikut contoh hasil output untuk opsi (C)ari :  
+  
+![OutputCari](https://user-images.githubusercontent.com/56189248/70881123-24562100-1ffe-11ea-8c0f-60e5f4e8d8dd.png)  
+  
+Berikut contoh hasil output untuk opsi (H)apus :  
+  
--- Tugas_Praktikum5.py
+++ Tugas_Praktikum5.py
@@ -1,7 +1,5 @@
 
 daftar = {}
-no = int(0)
-
 
 while True:
     print("")
@@ -13,21 +11,23 @@ while True:
         garis = 79
         print('=' * garis)
         print( '| No |    NIM    |          Nama          |  Tugas  |  UTS  |  UAS  |  Akhir  |')
-        print ('='*garis)
-        for i in daftar.items():
-            print('| {0:2d} |  {1:7d}  |  {2:20s}  |  {3:5d}  |  {4:3d}  |  {5:3d}  |  {6:5.2f}  |'.format(i[1][0], i[1][1], i[1][2], i[1][3], i[1][4], i[1][5], i[1][6]))
-            # print (i)
         print('=' * garis)
+        for no, i in zip (range (1,len(daftar)+1), daftar.items()) :
+            print('| {0:2d} |  {1:7d}  |  {2:20s}  |  {3:5d}  |  {4:3d}  |  {5:3d}  |  {6:5.2f}  |'
+                  .format(no, i[1][0], i[1][1], i[1][2], i[1][3], i[1][4], i[1][5]))
+        print('=' * garis)
+
     elif c.lower() == 't':
         print("Tambah Daftar")
-        no += 1
         nim = int(input('Masukkan NIM : '))
         nama = str(input('Masukkan Nama : '))
         tgs = int(input('Masukkan Nilai Tugas : '))
         uts = int(input('Masukkan Nilai UTS : '))
         uas = int(input('Masukkan Nilai UAS : '))
         akr = float((uts * 0.35) + (uas * 0.35) + (tgs * 0.3))
-        daftar [nim] = [no, nim, nama, tgs, uts, uas, akr]
+        daftar [nim] = [nim, nama, tgs, uts, uas, akr]
+        print ('\nData dengan NIM {0} telah berhasil dimasukkan kedalam Daftar Nilai'.format(nim))
+
     elif c.lower() == 'u':
         print("Ubah Daftar")
         nim = int(input('Masukkan NIM : '))
@@ -37,33 +37,38 @@ while True:
             uas = int(input('Masukkan Nilai UAS : '))
             tgs = int(input('Masukkan Nilai Tugas : '))
             akr = float((uts * 0.35) + (uas * 0.35) + (tgs * 0.3))
-            daftar [nim] = [no, nim, nama, tgs, uts, uas, akr]
+            daftar [nim] = [nim, nama, tgs, uts, uas, akr]
+            print('\nData dengan NIM  {0} telah berhasil diubah'.format(nim))
         else:
-            print("NIM {0} yang anda input tidak ada atau salah".format(nim))
+            print("\nNIM {0} yang anda input tidak ada atau salah".format(nim))
+
     elif c.lower() == 'h':
         print("Hapus Daftar")
         nim = int(input("NIM: "))
         if nim in daftar.keys():
             del daftar[nim]
+            print('\nData dengan NIM {0} telah berhasil dihapus dari Daftar Nilai'.format(nim))
         else:
-            print("NIM {0} yang anda input tidak ada atau salah".format(nim))
+            print("\nNIM {0} yang anda input tidak ada atau salah".format(nim))
+
     elif c.lower() == 'c':
         print("Cari Daftar")
-        nim = int(input("NIM: "))
+        nim = int(input("Masukkan NIM: "))
         if nim in daftar.keys():
             print("NIM {0} adalah  \n"
                   "Nama         : {1}\n"
                   "Nilai Tugas  : {2}\n"
                   "Nilai UTS    : {3}\n"
                   "Nilai UAS    : {4}\n"
-                  "Nilai Akhir  : {5}\n"
+                  "Nilai Akhir  : {5:.2f}\n"
             .format(nim,
+                    daftar[nim][1],
                     daftar[nim][2],
                     daftar[nim][3],
                     daftar[nim][4],
-                    daftar[nim][5],
-                    daftar[nim][6]))
+                    daftar[nim][5]))
         else:
-            print("NIM {0} yang anda input tidak ada atau salah".format(nim))
+            print("\nNIM {0} yang anda input tidak ada atau salah".format(nim))
+
     else:
-        print("Pilih menu yang tersedia")
+        print("\nMenu yang anda pilih tidak tersedia, silahkan pilih menu yang tersedia")
